81240ca5f59d7751106742eeaece788ef4b4b80b
Manasi2804 <shahmanasi281996@gmail.com>
2020-05-15 23:18:59 +0530

    ADDED:Test case 1.2 Given feet value null return false

--- src/test/java/com/bl/quantitymeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/bl/quantitymeasurement/QuantityMeasurementTest.java
@@ -1,16 +1,26 @@
 package com.bl.quantitymeasurement;
 
+
+import com.bl.quantitymeasurment.Feet;
 import com.bl.quantitymeasurment.QuantityMeasurement;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class QuantityMeasurementTest {
     QuantityMeasurement quantityMeasurement = new QuantityMeasurement();
+
     @Test
-    public void givenFeetValue_WhenEqual_ShouldReturnTrue() {
+    public void givenFeetValues_WhenEqual_ShouldReturnTrue() {
         Feet f1 = new Feet(0.0);
         Feet f2 = new Feet(0.0);
         String result1 = quantityMeasurement.compare(f1, f2);
         Assert.assertEquals("equal", result1);
+    }
+    @Test
+    public void givenTwoFeetObjects_WhenOneOfThemIsNull_ShouldReturnFalse() {
+        Feet f1 = new Feet(0.0);
+        Feet f2 = null;
+        String result1 = quantityMeasurement.compare(f1, f2);
+        Assert.assertEquals("not equal", result1);
     }
 }
